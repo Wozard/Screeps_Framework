9ad43cfbf7ca4f1392f11bba1f5b4a5f0dfc457a
Gary Dunn <g_dunn@u.pacific.edu>
2016-08-27 15:58:10 -0700

    Edited tabbing (very important)

--- ai_harvester.js
+++ ai_harvester.js
@@ -3,23 +3,22 @@ var utils = require('ai_utils');
 var counts = require('unit_counts');
 
 module.exports = {
-    run: function() { harvTick() }
+   run: function() { harvTick() }
 };
 
 function harvTick() {
-  //Grab list of active builders
-  var harvList = counts.list('harvester');
+   //Grab list of active builders
+   var harvList = counts.list('harvester');
 
 
-  for (var creep of harvList) {
-    var curCarried = _.sum(creep.carry);
+   for (var creep of harvList) {
+      var curCarried = _.sum(creep.carry);
 
-    //Mine until full, store energy in spawns and exts
-    if (curCarried < creep.carryCapacity) {
-        utils.mineEnergy(creep);
-    } else {
-        utils.storeEnergy(creep);
-    }
-  }
-
+      //Mine until full, store energy in spawns and exts
+      if (curCarried < creep.carryCapacity) {
+         utils.mineEnergy(creep);
+      } else {
+         utils.storeEnergy(creep);
+      }
+   }
 }
